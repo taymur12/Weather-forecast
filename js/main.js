9c481948ed2d00b06c833455e54c68da82f0e423
Taymur <htaymur12@gmail.com>
2021-08-29 23:48:33 +0600

    added logo

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const weatherbtn = () =>{
     .then(Data => displayWeather(Data))
 }
 
-weatherbtn()
+
 
 const displayWeather = weatherForecast =>{
 
@@ -35,85 +35,3 @@ const displayWeather = weatherForecast =>{
     cardid.appendChild(div)
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const weatherbtn = () =>{
-//     const inputField = document.getElementById('input-Field')
-//     const inputFieldText = inputField.value
-
-//     inputField.value = ''
-//     const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputFieldText}&appid=edd4505bafbfa147a6109937ce0afea7`
-//     fetch(url)
-//     .then(res => res.json())
-//     .then(Data => displayWeather(Data.main))
-// }
-
-// const displayWeather = weatherForecast =>{
-//     const cardid = document.getElementById('cardID')
-//     cardid.textContent =''
-//     const div = document.createElement('div')
-//     div.classList.add('carddiv')
-//     div.innerHTML = `<div  class="card mx-auto" style="max-width: 18rem;">
-//     <div class="card-header">Weather Today</div>
-//     <div class="card-body text-dark">
-//       <h5 class="card-title">Temperature: ${weatherForecast.temp}</h5>
-//       <span class="card-text">Temperature-Max ${weatherForecast.temp_min}</span>
-//       <span class="card-text">Temperature-Max ${weatherForecast.temp_max}</span>
-//       <p class="card-text">Humidity: ${weatherForecast.humidity}</p>
-//       <p class="card-text">predict: ${weatherForecast.main}</p>
-//     </div>
-//     </div> 
-//     `
-//     cardid.appendChild(div)
-    
-// }
